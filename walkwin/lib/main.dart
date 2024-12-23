d96dd847399c2a66e84d280096c27b5a601d25fa
NikosK10 <nikoskatsaidonis@gmail.com>
2024-12-23 13:34:46 +0200

    sign in

--- walkwin/lib/main.dart
+++ walkwin/lib/main.dart
@@ -1,10 +1,10 @@
-import 'screens/home_page.dart';
-import 'screens/store_page.dart';
+/*
 import 'screens/challenges_page.dart';
 import 'screens/profile_page.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'screens/sign_in_page.dart';
 
 
 void main() {
@@ -36,20 +36,27 @@ class MyApp extends StatelessWidget {
         '/': (context) => const HomePage(),
         '/store': (context) => const StorePage(),
         '/challenges': (context) => const Challenges(),
-        '/profile': (context) => const Profile()
+        '/profile': (context) => const Profile(),
+        '/signin': (context) => const SignIn()
       },
     );
   }
 }
 
-/*
+*/
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'screens/profile_page.dart'; // Make sure the ProfilePage is correctly imported.
+import 'screens/sign_in_page.dart'; // Make sure the ProfilePage is correctly imported.
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),);
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -57,11 +64,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Profile Page',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: const ProfilePage(), // Launch the ProfilePage
+      home: const SignIn(), // Launch the ProfilePage
     );
   }
 }
-*/
